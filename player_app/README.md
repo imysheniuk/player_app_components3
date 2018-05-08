bc8601a5715b53329e8750adedf41e44884aea43
imysheniuk <i.mysheniuk@gmail.com>
2018-05-08 16:18:58 +0300

    Add files via upload

---
+++ player_app/README.md
@@ -0,0 +1,16 @@
+
+# Player created
+
+It can:
+
+* Display playlist
+* Play song, by dbclick on it's name
+* Delete song from list (with confirmation)
+* Add new song, by some absolute url address 
+* Playlist repeat mode (When last song finished = > the first one need to start, if you       choose some in the middle, next one need to play)  
+* Local Storage used (not to lose added songs after browser was closed)
+
+### My short description
+I have an idea how to do some EventListeners with $scope.on, but need a little more time to sort it out. I can do it if needed.
+
+I also used Local Storage but it works different on different laptops (I don't know the reason). So, to make sure you can open this app in your browser, I just comented it (src > app > app.js line: 38-41)
---
+++ player_app/src/app/app.js
@@ -0,0 +1,110 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import angular from 'angular';
+import routing from '../app.config'
+import '../style/app.css';
+
+import appComponent from './components/app.component';
+import {AppCtrl} from './components/app.component';
+
+import addSongComponent from './components/addSong/addSong.component';
+import {AddSongCtrl} from './components/addSong/addSong.component';
+
+  const MODULE_NAME = 'app';
+
+  angular.module(MODULE_NAME, [])
+    .directive('app', appComponent)
+    .controller('AppCtrl', AppCtrl)
+    .directive('addSong', addSongComponent)
+    .controller('AddSongCtrl', AddSongCtrl);
+
+export default MODULE_NAME;
+
+
+
+
+
+
+
+
+
+// let app = () => {
+//   return {
+//     template: require('./components/app.html'),
+//     controller: 'AppCtrl',
+//     controllerAs: 'app'
+//   }
+// };
+
+// const MODULE_NAME = 'app';
+
+// angular.module(MODULE_NAME, [])
+//   .directive('app', app)
+//   .controller('AppCtrl', function($scope) {
+//     $scope.songs = [
+//       {name: 'SoundHelix Song 1', adress: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"},
+//       {name: 'SoundHelix Song 2', adress: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3"},
+//       {name: 'SoundHelix Song 3', adress: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3"},
+//       {name: 'SoundHelix Song 4', adress: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3"},
+//       {name: 'SoundHelix Song 5', adress: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3"},
+//       {name: 'SoundHelix Song 6', adress: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3"},
+//       {name: 'SoundHelix Song 7', adress: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3"},
+//       {name: 'SoundHelix Song 8', adress: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3"}
+      
+//     ];
+
+    // $scope.removeItem = (song) => {
+    //   let answer = confirm("Are you sure to delete a song?")
+    //   if (answer==true) {
+    //     let index = $scope.songs.indexOf(song);    
+    //     $scope.songs.splice(index, 1);
+    //   }
+    // }
+
+    // // $scope.init = () => {
+    // //   $scope.songs = JSON.parse(localStorage.getItem('playlist'));
+    // // };
+    // // $scope.init();
+
+    // $scope.newSong = {};
+
+    // $scope.addSong = (newSong) => {
+    //   $scope.songs.push(newSong);
+    //     localStorage.setItem('playlist', JSON.stringify($scope.songs));
+    //     $scope.newSong = {};
+    // };
+
+    // $scope.dblPlay = (event) => {
+    //   event.target.parentElement.children[1].children[0].play();
+    // };
+
+    // let findNodeIndex = (node) => {
+    //   let i = 0;
+    //   while (node = node.previousSibling){
+    //     if (node.nodeType === 1) { ++i }
+    //   }
+    //   return i;
+    // };
+
+    // document.addEventListener('play', function(event){
+    //   let audios = document.getElementsByTagName('audio');
+    //   for(let i = 0, len = audios.length; i < len; i++){
+    //     if(audios[i] !== event.target){
+    //       audios[i].pause();
+    //     }
+    //   }
+    // }, true);
+
+    // document.addEventListener('ended', function(event){
+    //   let audios = document.getElementsByTagName('audio');
+    //   let currentSongId = findNodeIndex(event.target.parentElement.parentElement);
+    //   if(currentSongId == (audios.length -1)){
+    //     audios[0].play();
+    //   } else {
+    //     audios[currentSongId + 1].play();        
+    //   }
+    // }, true);
+
+    // $scope.on('ended', args => {
+    // })
+    // $scope.emit('ended', {index: currentSongId});
+  // });
